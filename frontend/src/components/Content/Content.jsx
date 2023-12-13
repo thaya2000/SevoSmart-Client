9f076860fa29697014554b57f90b0519442ee617
Thavamohan Thananchayan <thananchayan26@gmail.com>
2023-12-13 23:30:37 +0530

    text animation

--- frontend/src/components/Content/Content.jsx
+++ frontend/src/components/Content/Content.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect, useRef } from "react";
-import ScrollTrigger from "react-scroll-trigger";
+import React, { useState, useEffect } from "react";
 import "./Content.css";
-import SolarBg from "../../images/SolarBg.png";
-import ConstructionsBg from "../../images/ConstructionsBg.jpg";
 
 const Content = () => {
+  const [typewriterText, setTypewriterText] = useState("");
+  const [startTypewriter, setStartTypewriter] = useState(false);
+  const targetText = "Sri Lanka.";
+
+  useEffect(() => {
+    if (startTypewriter) {
+      const typeText = async () => {
+        for (let i = 0; i < targetText.length; i++) {
+          setTypewriterText((prevText) => prevText + targetText.charAt(i));
+          await new Promise((resolve) => setTimeout(resolve, 100));
+        }
+      };
+
+      typeText();
+    }
+  }, [startTypewriter, targetText]);
+
+  useEffect(() => {
+    // Start the typewriter effect after a delay
+    const timeoutId = setTimeout(() => {
+      setStartTypewriter(true);
+    }, 1500);
+
+    // Cleanup function to clear the timeout when the component unmounts
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <div className="content">
       <div className="contentTitle">
         <span>Delivering the Best</span>
         <span>Smart Home solutions in</span>
-        <span>Sri Lanka.</span>
+        <span>{typewriterText}</span>
       </div>
 
       <div className="Accessories"></div>
