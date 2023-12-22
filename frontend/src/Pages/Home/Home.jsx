ad4de8fd80e0e8a3a09f5ce4b8bd716a8ebd7aa9
Tharmabalan Nevaashahan <79234817+Nevaashahan@users.noreply.github.com>
2023-12-22 11:32:42 +0530

    Home-scroll-01

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { debounce } from "lodash";
 import Navigation from "../../Components/Navigation/Navigation.jsx";
 import ContentSolar from "../../Components/Content/ContentSolar";
 import ContentConstructions from "../../Components/Content/ContentConstructions";
@@ -7,61 +8,79 @@ import "./Home.css";
 import Content from "../../Components/Content/Content";
 
 const Home = () => {
-  const contentRef = useRef(null);
-  const contentSolarRef = useRef(null);
-  const contentConstructionsRef = useRef(null);
+  const contentRefs = [useRef(null), useRef(null), useRef(null)];
+  const [activeSlide, setActiveSlide] = useState(0);
 
-  const handleWheelScroll = (event) => {
+  const handleWheelScroll = debounce((event) => {
     const deltaY = event.deltaY;
+    const scrollDelta = deltaY > 0 ? 1 : -1;
+    const newSlide = Math.max(0, Math.min(activeSlide + scrollDelta, 2));
 
-    if (deltaY > -0.75) {
-      // Scroll down to the next section
-      if (
-        contentSolarRef.current &&
-        window.scrollY < contentSolarRef.current.offsetTop
-      ) {
-        contentSolarRef.current.scrollIntoView({ behavior: "smooth" });
-      } else if (
-        contentConstructionsRef.current &&
-        window.scrollY < contentConstructionsRef.current.offsetTop
-      ) {
-        contentConstructionsRef.current.scrollIntoView({ behavior: "smooth" });
-      }
-    } else {
-      // Scroll up to the previous section
-      if (
-        contentSolarRef.current &&
-        window.scrollY < contentSolarRef.current.offsetTop
-      ) {
-        contentRef.current.scrollIntoView({ behavior: "smooth" });
-      } else if (
-        contentConstructionsRef.current &&
-        window.scrollY < contentConstructionsRef.current.offsetTop
-      ) {
-        contentSolarRef.current.scrollIntoView({ behavior: "smooth" });
-      }
+    if (newSlide !== activeSlide) {
+      setActiveSlide(newSlide);
+
+      // Manually scroll to the new slide position
+      const targetPosition = contentRefs[newSlide].current.offsetTop;
+
+      // Use smooth scroll to the target position
+      smoothScrollTo(targetPosition);
     }
+  }, 800); // Adjust the debounce time as needed
+
+  const smoothScrollTo = (targetPosition) => {
+    const currentPosition = window.scrollY;
+    const distance = targetPosition - currentPosition;
+
+    const startTime = performance.now();
+    const duration = 800; // Adjust as needed
+
+    const animateScroll = (currentTime) => {
+      const elapsedTime = currentTime - startTime;
+      const progress = Math.min(elapsedTime / duration, 1);
+      const easing = easeInOutQuad(progress);
+      window.scrollTo(0, currentPosition + distance * easing);
+
+      if (progress < 1) {
+        requestAnimationFrame(animateScroll);
+      }
+    };
+
+    requestAnimationFrame(animateScroll);
   };
 
+  const easeInOutQuad = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t);
+
   useEffect(() => {
-    window.addEventListener("wheel", handleWheelScroll);
+    const handleScroll = () => {
+      const currentSlide = Math.floor(window.scrollY / window.innerHeight);
+      setActiveSlide(currentSlide);
+    };
+
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("wheel", handleWheelScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   return (
-    <div className="Home">
+    <div className="Home" onWheel={handleWheelScroll}>
       <Navigation />
-      <Content ref={contentRef} />
-      <div ref={contentSolarRef}>
-        <ContentSolar />
+
+      <div className="slides-container">
+        <div ref={contentRefs[0]}>
+          <Content />
+        </div>
+        <div ref={contentRefs[1]}>
+          <ContentSolar />
+        </div>
+        <div ref={contentRefs[2]}>
+          <ContentConstructions />
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
-      <div ref={contentConstructionsRef}>
-        <ContentConstructions />
-      </div>
-      <Footer />
     </div>
   );
 };
