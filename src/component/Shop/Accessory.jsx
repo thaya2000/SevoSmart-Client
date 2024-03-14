d1f425dd83ae2b43a320ad4eb80dccfaa63c77b4
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-03-14 12:58:47 +0530

    Resolve merge conflict

--- src/component/Shop/Accessory.jsx
+++ src/component/Shop/Accessory.jsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Accessory = ({ accessory_image, accessory_name, accessory_price }) => {
   return (
-    <div className="flex flex-col items-center border-1 border-black rounded-2xl w-80">
+    <div className="flex flex-col items-center border-2 border-black rounded-2xl w-80">
       <img
         className="flex justify-self-center mt-2 py-3 h-60 w-60"
         src={accessory_image}
         alt={accessory_name}
       />
       <div className="flex justify-items-center text-4xl font-bold py-3">
-
         {accessory_name}
       </div>
       <div className="flex justify-items-center text-4xl font-bold py-3">
