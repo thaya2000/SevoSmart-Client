323750c29a1618c829c4838bd30bdf870f67cbd1
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-05-17 19:08:30 +0530

    refactor : refactor util folder

--- src/util/constants.dev.jsx
+++
@@ -1,6 +0,0 @@
-// constants.dev.jsx
-export const DEFAULT_EMAIL = "user@gmail.com";
-export const DEFAULT_PASSWORD = "user1234";
-export const DEFAULT_FIRSTNAME = "user_f";
-export const DEFAULT_LASTNAME = "user_l";
-export const DEFAULT_ROLE = "USER";
