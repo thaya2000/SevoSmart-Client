5bfad58fb4367b39dfe203d6c4d8574a281f15ff
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-05-19 19:43:05 +0530

    feat : When fetching data using Etag

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
 import { AuthProvider } from "./context/authContext.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../src/redux/store/configureStore";
+// import store from "./redux/store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
+      <Provider store={store}>
+        <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
     </AuthProvider>
   </React.StrictMode>
 );
